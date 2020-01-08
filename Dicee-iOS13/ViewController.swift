91c3b083a30786d20205ec583d1c0c781be58f90
falcon <falcon_chen@qq.com>
2020-01-08 17:56:02 +0800

    final completed

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -12,31 +12,17 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
-    let diceNumberImages = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo") ,#imageLiteral(resourceName: "DiceThree") ,#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
-    var leftDiceNumber = 0
     
     
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        showDiceImage()
-    }
-
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-                                        
-        leftDiceNumber+=1
-        if leftDiceNumber == 6{
-            leftDiceNumber = 0
-        }
-        showDiceImage()
         
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo") ,#imageLiteral(resourceName: "DiceThree") ,#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+        //diceImageView1.image = diceArray[Int.random(in:0...5)]
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
         
     }
-     func showDiceImage(){
-        diceImageView1.image = diceNumberImages[leftDiceNumber]
-        diceImageView2.image = diceNumberImages[5-leftDiceNumber]
-    }
+    
 }
 
